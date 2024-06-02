4b4d64d7eda0d5c92421bd415748c8ffd28d031d
Prabin Pokhrel <prabinpokhrel7722@gmail.com>
2024-06-02 22:35:01 +0545

    update

--- index.js
+++ index.js
@@ -1,26 +1,87 @@
-const ulist= document.querySelector('ul');
-const inputs= document.querySelector('input');
-const button= document.querySelector('button');
+const cells = document.querySelectorAll('.cell');
+const reset = document.querySelector('.reset');
+const playerTurn = document.querySelector('.playerturn');
+const msg = document.querySelector('.msg');
+const newGame = document.querySelector('.newgame');
+const newGameBtn = document.querySelector('.newgamebtn');
 
-button.addEventListener('click', () =>{
-    const myItem = inputs.value; 
-    inputs.value ='';
+let turnX = true;
+let count = 0;
+playerTurn.innerHTML = "Player X's Turn";
 
-    const list = document.createElement('li');
-    const lispan = document.createElement('span');
-    const libutton = document.createElement('button');
+cells.forEach((cell) => {
+  cell.addEventListener('click', () => {
+    if (turnX) {
+      playerTurn.innerHTML = "Player O's Turn";
+      cell.innerHTML = "X";
+      turnX = false;
+    }
+    else {
+      playerTurn.innerHTML = "Player X's Turn";
+      cell.innerHTML = "O";
+      turnX = true;
+    }
+    cell.disabled = true;
+    count++;
+    let isWin = checkWinner();
 
-    list.appendChild(lispan);
-    lispan.textContent = myItem;
-
-    list.appendChild(libutton);
-    libutton.textContent = 'Delete';
-
-    ulist.appendChild(list);
-
-    libutton.addEventListener('click', () => {
-        ulist.removeChild(list);
-    });
-inputs.focus();
+    if (count === 9 && !isWin) {
+      drawGame();
+    }
+  });
 });
+
+function drawGame() {
+  playerTurn.innerHTML = "Play Again-";
+  msg.innerHTML = "It's Draw Try Again";
+  cell.disabled = true;
+}
+
+function checkWinner() {
+  const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  winPatterns.forEach((pattern) => {
+
+    let position1 = cells[pattern[0]].innerHTML;
+    let position2 = cells[pattern[1]].innerHTML;
+    let position3 = cells[pattern[2]].innerHTML;
+
+    if (position1 != '' && position2 != '' && position3 != '') {
+      if (position1 === position2 && position2 === position3) {
+        showWinMsg(position1);
+        return true;
+      }
+    }
+
+  });
+}
+
+function showWinMsg(winner) {
+  playerTurn.innerHTML = "Congrulations";
+  msg.innerHTML = `Winner is ${winner}`;
+  cells.forEach((cell) => {
+    cell.disabled = true;
+  });
+}
+
+function resetBTn() {
+  turnX = true;
+  cells.forEach((cell) => {
+    cell.textContent = "";
+    msg.innerHTML = '';
+    cell.disabled = false;
+
+
+  });
+}
+reset.addEventListener('click', resetBTn);
+
 
